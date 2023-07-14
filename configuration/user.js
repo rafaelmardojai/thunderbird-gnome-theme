bd7f03b56f1f0b9c053cb8599ff7cdaea578236a
Rafael Mardojai CM <email@rafaelmardojai.com>
2023-07-14 08:40:23 -0500

    user.js: Update preferences

--- configuration/user.js
+++ configuration/user.js
@@ -1,16 +1,10 @@
 /* user.js
- * https://github.com/rafaelmardojai/firefox-gnome-theme/
+ * https://github.com/rafaelmardojai/thunderbird-gnome-theme/
  */
 
 // Enable customChrome.css
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// Set UI density to normal
-user_pref("browser.uidensity", 0);
-
 // Enable SVG context-propertes
 user_pref("svg.context-properties.content.enabled", true);
-
-// Disable private window dark theme
-user_pref("browser.theme.dark-private-windows", false);
 
