f99df57905f368661f410cdf44dc1e0d9b179cf4
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2026-02-22 11:23:09 +0200

    fix double-install regression

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-THEMEDIRECTORY=$(cd "$(dirname $0)" && cd .. && pwd)
+THEMEDIRECTORY=$(cd "$(dirname $0)" && cd ../.. && pwd)
 THUNDERBIRDFOLDER=~/.thunderbird
 PROFILENAME=""
 THEME="DEFAULT"
@@ -34,7 +34,7 @@ function saveProfile(){
 
 	# Copy theme repo inside
 	echo "Copying repo in $PWD" >&2
-	cp -fR "$THEMEDIRECTORY" "$PWD/thunderbird-gnome-theme" || { echo "FAIL, couldn't copy to $PWD/chrome, please check if there's something named 'chrome', that is not a dir."; exit 1; }
+	cp -fR "$THEMEDIRECTORY/thunderbird-gnome-theme" "$PWD/" || { echo "FAIL, couldn't copy to $PWD/chrome, please check if there's something named 'chrome', that is not a dir."; exit 1; }
 
 	# Create single-line user CSS files if non-existent or empty.
 	if [ -s userChrome.css ]; then
