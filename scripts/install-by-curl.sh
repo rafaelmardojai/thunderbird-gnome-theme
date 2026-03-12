2c038e4c6e28b4411d40349cbb57007bf8d8b183
Charles LESECQ <22601105+clesecq@users.noreply.github.com>
2026-03-12 10:00:58 +0100

    fix(scripts): replace firefox with thunderbird in install-by-curl

--- scripts/install-by-curl.sh
+++ scripts/install-by-curl.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-VERSION=$(curl --silent "https://api.github.com/repos/rafaelmardojai/firefox-gnome-theme/releases/latest" | grep tag_name | cut -d'"' -f4)
-FILENAME=firefox-gnome-theme-$VERSION.tar.gz
+VERSION=$(curl --silent "https://api.github.com/repos/rafaelmardojai/thunderbird-gnome-theme/releases/latest" | grep tag_name | cut -d'"' -f4)
+FILENAME=thunderbird-gnome-theme-$VERSION.tar.gz
 
 (
 
 cd $(mktemp -d) || exit 1
-mkdir firefox-gnome-theme
-cd firefox-gnome-theme
+mkdir thunderbird-gnome-theme
+cd thunderbird-gnome-theme
 
-curl -LJo $FILENAME https://github.com/rafaelmardojai/firefox-gnome-theme/tarball/$VERSION
+curl -LJo $FILENAME https://github.com/rafaelmardojai/thunderbird-gnome-theme/tarball/$VERSION
 
 tar -xzf $FILENAME --strip-components=1
 
